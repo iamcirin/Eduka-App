9ede20942dbd1e8fd5cef3d22b50725df249184a
jeff <jeff.salarium@gmail.com>
2022-09-27 12:51:56 +0800

    feat: dashboard initial layout

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import React from 'react'
-import './App.css';
-import Login from './components/Login';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import Register from './components/Register';
+import React from "react";
+import "./App.css";
+import Login from "./components/Login";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Register from "./components/Register";
+import Dashboard from "./components/Dashboard";
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Login/>}/>
-        <Route path='eduka/register' element={<Register/>}/>
+        <Route path="/" element={<Login />} />
+        <Route path="eduka/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
   );
-}
+};
 
-export default App
+export default App;
